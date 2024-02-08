6750e7d223f9e323aa3827f8fa044607c000bfd0
Tom Cowen <tomreescowen@gmail.com>
2024-02-08 15:16:58 +0000

    Add end of game text to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ let score = 0;
 let maxQuestions = 10;
 let acceptingAnswers = true;
 
+const allAnswersContainer = document.getElementById("all-answers");
 const questionElement = document.getElementById("question-container");
 const progressText = document.getElementById('progressText');
 const progressBarFull = document.getElementById('progressBarFull');
@@ -86,38 +87,38 @@ function selectAnswer(e) {
     if (!acceptingAnswers) {
         return;
     } else {
-    const selectedDiv = e.target;
-    const isCorrect = selectedDiv.dataset.correct === "true";
+        const selectedDiv = e.target;
+        const isCorrect = selectedDiv.dataset.correct === "true";
 
 
 
-    if (isCorrect) {
-        selectedDiv.classList.add("correct");
-        score++;
-        scoreTracker.textContent = score;
-    } else {
-        selectedDiv.classList.add("incorrect");
-    }
-
-    answerDivs.forEach(answerDiv => {
-        if (answerDiv.dataset.correct === "true") {
-            answerDiv.classList.add("correct");
+        if (isCorrect) {
+            selectedDiv.classList.add("correct");
+            score++;
+            scoreTracker.textContent = score;
         } else {
-            answerDiv.classList.add("incorrect");
+            selectedDiv.classList.add("incorrect");
         }
-        answerDiv.disabled = true;
-    });
 
-    if (currentQuestionCounter < maxQuestions) {
-        currentQuestionCounter++;
-        progressText.innerText = `Question ${currentQuestionCounter} / ${maxQuestions}`;
-        progressBarFull.style.width = `${(currentQuestionCounter / maxQuestions) * 100}%`;
+        answerDivs.forEach(answerDiv => {
+            if (answerDiv.dataset.correct === "true") {
+                answerDiv.classList.add("correct");
+            } else {
+                answerDiv.classList.add("incorrect");
+            }
+            answerDiv.disabled = true;
+        });
+
+        if (currentQuestionCounter < maxQuestions) {
+            currentQuestionCounter++;
+            progressText.innerText = `Question ${currentQuestionCounter} / ${maxQuestions}`;
+            progressBarFull.style.width = `${(currentQuestionCounter / maxQuestions) * 100}%`;
+        }
+
+        setTimeout(() => {
+            handleNextButton();
+        }, 500); // Adjust the delay time as needed (in milliseconds)
     }
-
-    setTimeout(() => {
-        handleNextButton();
-    }, 500); // Adjust the delay time as needed (in milliseconds)
-}
 }
 
 
@@ -141,6 +142,23 @@ function changeAcceptingAnswers(newVal) {
 function showScore() {
     resetState();
     questionElement.innerHTML = `<h2>You scored ${score} out of ${questions.length}!</h2>`
+    allAnswersContainer.innerHTML = `
+    <div class="answers-container col-lg-6">
+    <div id="answer-one" class="answer-container">
+    <p class="final-text">You</p>
+    </div>
+    <div id="answer-two" class="answer-container">
+    <p class="final-text">are</p>
+    </div>
+</div>
+<div class="answers-container col-lg-6">
+    <div id="answer-three" class="answer-container">
+    <p class="final-text">a</p>
+    </div>
+    <div id="answer-four" class="answer-container">
+    <p class="final-text">legend!</p>
+    </div>
+    `;
     changeAcceptingAnswers(false);
 };
 
